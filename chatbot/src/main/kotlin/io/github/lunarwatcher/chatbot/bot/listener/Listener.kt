bf872730e92330db5b1c248904abfa88cabac405
Olivia Zoe <zoe.i2k1@gmail.com>
2018-07-04 12:35:22 +0200

    Lombok is now removed. Closes #53

--- chatbot/src/main/kotlin/io/github/lunarwatcher/chatbot/bot/listener/Listener.kt
+++ chatbot/src/main/kotlin/io/github/lunarwatcher/chatbot/bot/listener/Listener.kt
@@ -4,7 +4,7 @@ import io.github.lunarwatcher.chatbot.bot.chat.Message
 import io.github.lunarwatcher.chatbot.bot.chat.ReplyMessage
 import io.github.lunarwatcher.chatbot.bot.command.CommandGroup
 
-//TODO add listeners
+
 interface Listener{
     val name: String;
     val description: String;
