aac26249b62c800d030a2ecdc2e396a7f07bec7c
Olivia Zoe <zoe.i2k1@gmail.com>
2018-10-03 22:00:05 +0200

    Optimizations, API clarifications.

--- Network/bot.py
+++ Network/bot.py
@@ -1,22 +1,21 @@
 import argparse
 import os
-import sys
 from multiprocessing.dummy import Pool
 
-import Config
-import datasetUtils as data
-import grammarRules as grammar
 import tensorlayer as tl
 from sklearn.utils import shuffle
 from tensorlayer.layers import *
 
-import Commands as cmd
+import datasetUtils as data
+import grammarRules as grammar
 
 
 # noinspection PyShadowingNames,PyAttributeOutsideInit
+# TODO improve the performance and memory use of the net, which currently is pretty big
 class Bot():
-    batchSize = 32
-    embedDim = 512
+    # Hyperparams
+    batchSize = 16
+    embedDim = 128
     # This is true by default to make it possible to run on 2 gig VRAM systems.
     embeddingCpu = True
 
@@ -44,6 +43,7 @@ class Bot():
 
     def idx(self, n: str):
         return self.w2idx[n]
+
     @staticmethod
     def getEpochs():
         while True:
@@ -53,6 +53,7 @@ class Bot():
             except ValueError:
                 print("Invalid int")
         return epochs
+
     @staticmethod
     def cleanInput(string: str):
 
@@ -63,11 +64,11 @@ class Bot():
         for epoch in range(epochs):
             epochTime = time.time()
             print("Time taken")
-            trainX, trainY = shuffle(self.trainX, self.trainY)# Passing random_state with a value != None gives a seed
+            trainX, trainY = shuffle(self.trainX, self.trainY)  # Passing random_state with a value != None gives a seed
             print("Data shuffled")
             avgLoss, iterations = 0, 0
 
-            for X, Y in tl.iterate.minibatches(inputs=trainX, targets=trainY, batch_size=self.batchSize, shuffle = False):
+            for X, Y in tl.iterate.minibatches(inputs=trainX, targets=trainY, batch_size=self.batchSize, shuffle=False):
 
                 X = tl.prepro.pad_sequences(X)
                 targetSeqs = tl.prepro.sequences_add_end_id(Y, end_id=self.endId)
@@ -82,28 +83,30 @@ class Bot():
                                                                        self.targetMask: tMask})
 
                 if iterations % 100 == 0:
-                    print("Epoch[%d/%d] step:[%d/%d] loss=%f" %(epoch + 1, epochs, iterations,
-                                                                           self.iterationsPerEpoch, iLoss))
+                    print("Epoch[%d/%d] step:[%d/%d] loss=%f" % (epoch + 1, epochs, iterations,
+                                                                 self.iterationsPerEpoch, iLoss))
 
                 avgLoss += iLoss
                 iterations += 1
 
-            print("Epoch[%d/%d] with average loss:%f took:%.5fs" % (epoch + 1, epochs, avgLoss/iterations, time.time()-epochTime))
+            print("Epoch[%d/%d] with average loss:%f took:%.5fs" % (
+                epoch + 1, epochs, avgLoss / iterations, time.time() - epochTime))
 
             tl.files.save_npz(self.net.all_params, name='bot.npz', sess=self.sess)
 
         # TODO tensorboard
 
-    def predict(self, input: str):
-        print("Input:" + input)
+    def predict(self, message: str):
+        print("Input:" + message)
         if not self.training and not self.initialized:
-            return "Sorry, I boot asyncronously. The neural net backend boots later than the bot itself. It should be up shortly :)"
+            return "Sorry, I boot asyncronously. The neural net backend boots later than the bot itself. It should be "\
+                   "up shortly :) "
 
-        if input.strip() == "":
+        if message.strip() == "":
             return "I don't know what to say."
-        input = self.cleanInput(input)
+        message = self.cleanInput(message)
         inputTokens = list()
-        for w in input.split(" "):
+        for w in message.split(" "):
             try:
                 inputTokens.append(self.w2idx[w])
             except KeyError:
@@ -111,13 +114,13 @@ class Bot():
 
         state = self.sess.run(self.netRnn.final_state_encode, {self.encodeSequences2: [inputTokens]})
         o, state = self.sess.run([self.sm, self.netRnn.final_state_decode],
-                            {self.netRnn.initial_state_decode: state, self.decodeSequences2: [[self.startId]]})
+                                 {self.netRnn.initial_state_decode: state, self.decodeSequences2: [[self.startId]]})
         wId = tl.nlp.sample_top(o[0], top_k=10)
         w = self.idx2w[wId]
         sentence = [w]
         for __ in range(30):
             o, state = self.sess.run([self.sm, self.netRnn.final_state_decode],
-                                {self.netRnn.initial_state_decode: state, self.decodeSequences2: [[wId]]})
+                                     {self.netRnn.initial_state_decode: state, self.decodeSequences2: [[wId]]})
             wId = tl.nlp.sample_top(o[0], top_k=3)
             w = self.idx2w[wId]
             if wId == self.endId:
@@ -132,7 +135,6 @@ class Bot():
         # Parse "unk"-only messages to "I don't understand" in some or another form.
         answer = ' '.join(sentence)
         return grammar.fix(answer)
-
 
     def model(self, encodeSequences, decodeSequences, training=True, reuse=False):
         with tf.variable_scope("model", reuse=reuse):
@@ -162,82 +164,33 @@ class Bot():
                                         embedding_size=self.embedDim, name="seqEmbedding")
         return netEncode, netDecode
 
-    def bootElsewhere(self):
-
-        cmd.PermissionManager.assumeAllAndInject()# Inject all the permissions before site boot
-        if not Config.isAnyEnabled(list(Config.enabledSites.keys())):
-            print("No sites are enabled! Shutting off...")
-            exit(-1)
-
-        if Config.isSiteEnabled("discord"):
-            import discordBot
-            discord = discordBot.Discord()
-            pool = Pool(processes=1)
-            pool.apply_async(discord.start, args=[self.predict])
-        if Config.isAnyEnabled(["stackoverflow.com", "stackexchange.com", "meta.stackexchange.com"]):
-            import stackexchange
-            stackexchange.Stackexchange.nnFun = self.predict
-            stackexchange.start()
-
+    def startChat(self):
         while True:
-            message = input(">> ")
-            if message == "~!save":
-                cmd.PermissionManager.saveAll()
-            elif(message == "~!stop" or message == "~!break" or message == "~!exit"):
-                cmd.PermissionManager.saveAll()
-                break
-            else:
-                print("Unknown command.")
-
-
-        if Config.isAnyEnabled(["stackoverflow.com", "stackexchange.com", "meta.stackexchange.com"]):
-            if "stackexchange" in sys.modules:
-                for site in stackexchange.Stackexchange.sites:
-                    site.stop()
-            else:
-                print("Site (stackexchange) enabled, but not imported!")
-
-        if Config.isSiteEnabled("discord"):
-            if "discord" in sys.modules:
-                discord.bot.logout()
-            else:
-                print("Site (discord) enabled, but not imported!")
-
-    def startChat(self, mode):
-
-        if mode == 0:
-
-            while True:
-                message = input("You >> ")
-                if message == "~!exit":
-                    self.sess.close()
-
-                    exit()
-                print("Bot >> " + self.predict(message))
-        elif mode == 1:
-            self.bootElsewhere()
-        else:
-            print("Unknown mode")
-            exit()
+            message = input("You >> ")
+            if message == "~!exit":
+                self.sess.close()
+                exit()
+            print("Bot >> " + self.predict(message))
 
     def startNet(self):
         print("Network starting")
         if self.training:
             # TODO not implemented: custom datasets
             # y if forced else input
-            dataset = "y" #input("Use the default dataset (y) or a custom one (filename, dataset/ is appended automatically)")
+            dataset = "y"  # input("Use the default dataset (y) or a custom one (filename, dataset/ is appended
+            # automatically)")
 
             if dataset == "y":
                 pass
             else:
                 pass
-            self.meta, self.idx_q, self.idx_a = data.loadData(PATH="dataset/")# This path
+            self.meta, self.idx_q, self.idx_a = data.loadData(PATH="dataset/")  # This path
             (trainX, trainY), (testX, testY), (validX, validY) = data.splitDataset(self.idx_q, self.idx_a)
 
             self.trainX = tl.prepro.remove_pad_sequences(trainX.tolist())
             self.trainY = tl.prepro.remove_pad_sequences(trainY.tolist())
-            self.testX  = tl.prepro.remove_pad_sequences(testX.tolist())
-            self.testY  = tl.prepro.remove_pad_sequences(testY.tolist())
+            self.testX = tl.prepro.remove_pad_sequences(testX.tolist())
+            self.testY = tl.prepro.remove_pad_sequences(testY.tolist())
             self.validX = tl.prepro.remove_pad_sequences(validX.tolist())
             self.validY = tl.prepro.remove_pad_sequences(validY.tolist())
 
@@ -263,7 +216,7 @@ class Bot():
 
             self.startId = self.w2idx["start_id"]
             self.endId = self.w2idx["end_id"]
-            if not "name_id" in self.idx2w:
+            if "name_id" not in self.idx2w:
                 # Since some earlier versions of the program itself created the dict without adding the
                 # name ID, this check is here to make sure it's present
                 print("Name ID not found. Appending and saving the dictionary...")
@@ -277,13 +230,19 @@ class Bot():
             self.xVocabSize = self.yVocabSize = len(self.idx2w)
         else:
             if not self.training:
-                print("The vocab files were not found in the dataset directory. (if changes were made to the directories, please change the code as well)")
-                print("Since training is set to false, none of the data required for this operation is present. Please train the bot before attempting to use")
-                print("It, or at the very least make sure the vocabs are present. The program will not exit. To avoid seeing this warning in the future, please")
+                print(
+                    "The vocab files were not found in the dataset directory. (if changes were made to the "
+                    "directories, please change the code as well)")
+                print(
+                    "Since training is set to false, none of the data required for this operation is present. Please "
+                    "train the bot before attempting to use")
+                print(
+                    "It, or at the very least make sure the vocabs are present. The program will not exit. To avoid "
+                    "seeing this warning in the future, please")
                 print("run the program in training mode")
                 exit()
-            self.w2idx = self.meta["w2idx"] # word to vector
-            self.idx2w = self.meta["idx2w"] # vector to word
+            self.w2idx = self.meta["w2idx"]  # word to vector
+            self.idx2w = self.meta["idx2w"]  # vector to word
             print(self.w2idx)
             self.xVocabSize = len(self.idx2w)
             self.startId = self.xVocabSize
@@ -291,13 +250,13 @@ class Bot():
             self.nameId = self.xVocabSize + 2
             self.w2idx["start_id"] = self.startId
             self.w2idx["end_id"] = self.endId
-            self.w2idx["name_id"] = self.nameId# The name ID is the one used in replies to be replaced with the bots name
+            self.w2idx[
+                "name_id"] = self.nameId  # The name ID is the one used in replies to be replaced with the bots name
 
             self.idx2w = self.idx2w + ["start_id", "end_id", "name_id"]
             self.xVocabSize = self.yVocabSize = self.xVocabSize + 3
 
             data.saveVocab(self.idx2w, self.w2idx)
-
 
         self.unkId = self.idx("unk")
         self.padId = self.idx("_")
@@ -329,7 +288,8 @@ class Bot():
 
         self.optimizer = tf.train.AdamOptimizer(learning_rate=learningRate).minimize(self.loss)
 
-        self.sess = tf.Session(config=tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True), log_device_placement=False))
+        self.sess = tf.Session(
+            config=tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True), log_device_placement=False))
         tl.layers.initialize_global_variables(self.sess)
         tl.files.load_and_assign_npz(sess=self.sess, name="bot.npz", network=self.net)
         self.initialized = True
@@ -340,27 +300,30 @@ class Bot():
             print("Neural network started")
             print("----------------------")
 
+
 def getBooleanInput(prompt):
     while True:
         try:
-            return {"true":True,"false":False, "t": True, "f": False, "1": True, "0": False,
-                    "True": True, "False" : False, "y" : True, "n": False, "yes" : True,
-                    "no" : False}[input(prompt).lower()]
+            return {"true": True, "false": False, "t": True, "f": False, "1": True, "0": False,
+                    "True": True, "False": False, "y": True, "n": False, "yes": True,
+                    "no": False}[input(prompt).lower()]
         except KeyError:
-            print("Invalid input please enter True or False!")
+            print("Invalid input. Please enter True or False!")
 
 
-def parseBoolean(str):
+def parseBoolean(string):
     try:
-        return {"true":True,"false":False, "t": True, "f": False, "1": True, "0": False,
-               "True": True, "False" : False, "y" : True, "n": False, "yes" : True,
-               "no" : False}[str]
-    except:
-        print("Invalid boolean: " + str)
+        return {"true": True, "false": False, "t": True, "f": False, "1": True, "0": False,
+                "True": True, "False": False, "y": True, "n": False, "yes": True,
+                "no": False}[string]
+    except KeyError:
+        print("Invalid boolean: " + string)
         return False
 
-def getIntInput(prompt, validChoices = []):
 
+def getIntInput(prompt, validChoices=None):
+    if validChoices is None:
+        validChoices = []
     while True:
         try:
             inp = input(prompt).lower()
@@ -386,7 +349,9 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--training", help="Am I training? (boolean)")
-    parser.add_argument("--mode", help="(When training = false only) Where to boot the bot. 0 is the console, 1 is online, 2 is online with Java/Kotlin. More modes may come eventually")
+    parser.add_argument("--mode",
+                        help="(When training = false only) Where to boot the bot. 0 is the console, 1 is the flask "
+                             "server, which can be used with the Java/Kotlin bot, or a different bot if wanted.")
     args = parser.parse_args()
     parser.print_help()
     trainingArg = args.training
@@ -406,7 +371,7 @@ if __name__ == '__main__':
     print("I'm gonna be " + ("training!" if training else "chatting"))
     mode = 0
     if not training:
-        mode = getIntInput("Mode: ", [0, 1, 2]) if modeArg is None else int(modeArg)
+        mode = getIntInput("Mode: ", [0, 1]) if modeArg is None else int(modeArg)
 
     tf.reset_default_graph()
 
@@ -416,9 +381,11 @@ if __name__ == '__main__':
         pool = Pool(processes=2)
         pool.apply_async(bot.startNet)
 
-        if mode == 2:
+        if mode == 1:
             from flask import Flask, jsonify, request
+
             app = Flask(__name__)
+
 
             @app.route("/predict", methods=["POST", "GET"])
             def getAnswer():
@@ -426,9 +393,10 @@ if __name__ == '__main__':
                 response = bot.predict(str(request.form.get("message")))
                 return jsonify(response)
 
+
             app.run(host="0.0.0.0", port=8213)
 
         else:
-            bot.startChat(mode)
+            bot.startChat()
     else:
         bot.startNet()
